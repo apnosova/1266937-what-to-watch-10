cb3837ce2b3f343c3041adc4f27f113d40b425e1
Angelina <angelinanosova@yandex.ru>
2022-07-18 06:19:46 +0300

    Добавляет React-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppScreenProps = {
+  numberOfMovies: number,
+  title: string,
+  genre: string,
+  year: number,
+}
+
+function App({ numberOfMovies, title, genre, year }: AppScreenProps): JSX.Element {
+  return (
+    <MainScreen
+      numberOfMovies={numberOfMovies}
+      title={title}
+      genre={genre}
+      year={year}
+    />
+  );
 }
 
 export default App;
