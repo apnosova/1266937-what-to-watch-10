498d51f5d58a85e5b1ffa4b6cdb4d22e279c705f
Angelina <angelinanosova@yandex.ru>
2022-08-26 13:30:06 +0300

    Создает новые компоненты

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -1,12 +1,12 @@
+import Header from '../../components/header/header';
 import MovieList from '../../components/movie-list/movie-list';
 import GenreList from '../../components/genre-list/genre-list';
+import Footer from '../../components/footer/footer';
 import { useAppSelector } from '../../hooks/hooks-index';
 import ShowMoreButton from '../../components/show-more-button/show-more-button';
-import Footer from '../../components/footer/footer';
 
 
 function MainScreen(): JSX.Element {
-
   const { moviesByGenre } = useAppSelector((state) => state);
 
   const promo = useAppSelector((state) => {
@@ -28,26 +28,7 @@ function MainScreen(): JSX.Element {
 
         <h1 className="visually-hidden">WTW</h1>
 
-        <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link" href="#todo">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <ul className="user-block">
-            <li className="user-block__item">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-              </div>
-            </li>
-            <li className="user-block__item">
-              <a className="user-block__link" href="#todo">Sign out</a>
-            </li>
-          </ul>
-        </header>
+        <Header />
 
         <div className="film-card__wrap">
           <div className="film-card__info">
@@ -94,7 +75,7 @@ function MainScreen(): JSX.Element {
 
         </section>
 
-        <Footer />
+        <Footer isMain />
 
       </div>
     </>
--- project/src/pages/sign-in-screen/sign-in-screen.tsx
+++ project/src/pages/sign-in-screen/sign-in-screen.tsx
@@ -1,8 +1,9 @@
 import { FormEvent, useRef } from 'react';
 import { useAppDispatch } from '../../hooks/hooks-index';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { AuthData } from '../../types/auth-data';
 import { login } from '../../store/api-actions';
+import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
 
 
@@ -18,7 +19,7 @@ function SignInScreen(): JSX.Element {
   };
 
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    // evt.preventDefault();
+    evt.preventDefault();
 
     if (loginRef.current !== null && passwordRef.current !== null) {
       onSubmit({
@@ -32,14 +33,7 @@ function SignInScreen(): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
+        <Logo />
         <h1 className="page-title user-page__title">Sign in</h1>
       </header>
 
